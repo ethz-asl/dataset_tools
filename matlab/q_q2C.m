159ca341fbb514eb7ecd5984c79d25972630de67
Janosch Nikolic <janosch.nikolic@mavt.ethz.ch>
2015-08-31 09:49:50 +0200

    Typo.

--- matlab/q_q2C.m
+++ matlab/q_q2C.m
@@ -9,9 +9,9 @@
 
 function C = q_q2C(q)
 
-q0 = q(1);
+qw = q(1);
 q_ = q(2:4);
 
-C = q0^2*eye(3)+2*q0*skewOp(q_)+skewOp(q_)^2 + q_*q_';
+C = qw^2*eye(3)+2*qw*skewOp(q_)+skewOp(q_)^2 + q_*q_';
 
 end
