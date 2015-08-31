dbb3713dad666644a29c227cd3645c0cc5eeb31c
Janosch Nikolic <janosch.nikolic@mavt.ethz.ch>
2015-08-31 09:49:39 +0200

    Catch cases without T_BS (e.g. PLY)

--- matlab/dataset_plot_body.m
+++ matlab/dataset_plot_body.m
@@ -19,11 +19,16 @@ hold on;
 % plot sensors
 for iSensor = 1:NSensor
   sensor = body.sensor{iSensor};
-  p_BS_B = sensor.T_BS(1:3, 4);
-  C_BS = sensor.T_BS(1:3, 1:3);
-  q_SB = q_C2q(C_BS);
-  q_plotPose(p_BS_B, q_SB, sensor.name, 0.015);
-  plot3([0, p_BS_B(1)], [0, p_BS_B(2)], [0, p_BS_B(3)], 'k');
+  if(isfield(sensor, 'T_BS'))
+    p_BS_B = sensor.T_BS(1:3, 4);
+    C_BS = sensor.T_BS(1:3, 1:3);
+    q_SB = q_C2q(C_BS);
+    q_plotPose(p_BS_B, q_SB, sensor.name, 0.015);
+    plot3([0, p_BS_B(1)], [0, p_BS_B(2)], [0, p_BS_B(3)], 'k');
+    disp(['     plotting sensor [', sensor.name, ']']);
+  else
+    disp(['     detected data without body-sensor extrinsics [', ...
+      sensor.name, ']']);
 end
 
 end
